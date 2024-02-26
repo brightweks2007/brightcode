144f60a5c7c35dd372db23c940d10b038a96f186
Brightweksweb <147143511+brightweks2007@users.noreply.github.com>
2024-02-26 17:34:56 +0100

    Add files via upload

---
+++ behaviuor/act.js
@@ -0,0 +1,18 @@
+function myfunction(){
+    let password = ("welcome")
+    document.getElementById("Rnd.text").innerHTML = "welcome"
+}
+
+function Btn() {
+    var email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    if ( email == "brightwekulom94@gmail.com" || password == '1234567') {
+        alert('Please input both your password and username')
+       }
+    } 
+
+alert("welcome to ndu login portal")
+function Btn() {
+    alert('welcome to your account')
+ var notify = document.getElementById('notify').value 
+}
